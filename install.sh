1b8937245208a3ab85cee1620c11a4c2fb92c07e
Domas <domas.bitvinskas@me.com>
2015-02-06 02:20:59 +0200

    Installer exists while the package does not

---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# closeheat install script
+#
+# Thanks for having a look at the script!
+# If you would like to install closeheat, just open up your terminal and type:
+#
+#    curl https://install.closeheat.com/<access_token> | sh
+#
+
+ACCESS_TOKEN='<access_token>'
+# We want to run the script only when it is fully download.
+# Wrapping it in function helps with that.
+download_finished () {
+
+echo '=> Installing closeheat toolkit.'
+# We will install a closheat npm package and set up your ~/.closeheat config
+# file with your access token.
+
+set -e
+set -u
+
+# Let's display everything on stderr.
+exec 1>&2
+
+# Check if npm exists.
+command -v npm >/dev/null && continue || {
+  cat <<"EOF"
+
+  Node Package Manager (npm) missing.
+
+    The best way to install npm is to install node.js.
+    npm is part of that package.
+
+  Download node.js installer:
+
+    http://nodejs.org
+
+EOF
+exit 1;
+}
+
+echo
+echo '   Installing closeheat npm package...'
+echo '     npm install -g closeheat'
+# Try to install without sudo
+# Will try with sudo if fails later
+# npm install -g closeheat
+echo "   closeheat npm package installed."
+
+touch ~/.closeheat
+echo "{\n  \"access_token\": \"$ACCESS_TOKEN\"\n}" > ~/.closeheat
+
+echo
+echo "   closeheat configuration saved to $HOME/.closeheat"
+
+echo
+echo "Installation successful."
+echo "------------------------"
+
+cat <<"EOF"
+
+Your list of apps deployed to closeheat:
+
+  * getgymnast - last deployed 10 minutes ago.
+    Develop quickly with:
+    git clone git@github.com:ryzaskaciukas/getgymnast.git && cd getgymnast && closeheat
+
+To create a new app:
+
+  $ closeheat create ~/awesome_app
+  $ cd ~/awesome_app
+  $ closeheat
+
+EOF
+
+trap - EXIT
+}
+
+download_finished
